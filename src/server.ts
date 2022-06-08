09eaccbf07b00ac0c32df38de8d3ed0247000a5b
Neto Sousa <netosousa29@yahoo.com>
2022-06-07 22:14:15 -0300

    Base do projeto e inclusao do README

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import routes from './index';
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello GoStack '});
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+    console.log('😎 Server started on port 3333!');
+});
