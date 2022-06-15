70d664e53f9ac87c825f0fd763a237c98d7ff9dd
Neto Sousa <netosousa29@yahoo.com>
2022-06-15 18:27:44 -0300

    Tratando o back-end do app, Relacionamento nos models

--- src/database/migrations/1655320137384-CreateUsers.ts
+++ src/database/migrations/1655320137384-CreateUsers.ts
@@ -8,7 +8,7 @@ export class CreateUsers1655320137384 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary:true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
