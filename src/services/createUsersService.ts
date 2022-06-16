f78c54b8108fba8f0b1b9f81f52b173a0681a239
Neto Sousa <netosousa29@yahoo.com>
2022-06-16 16:38:36 -0300

    Tratando o back-end do app, Criptografia de senha

--- src/services/createUsersService.ts
+++ src/services/createUsersService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import User from '../models/User';
+import { hash } from 'bcryptjs'
 
 interface Request {
     name: string;
@@ -19,10 +20,12 @@ class CreateUserService {
             throw new Error('Email adress already used.');
         }
 
+        const hashedPassword = await hash(password, 8)
+
         const user = usersRepository.create({
             name,
             email,
-            password,
+            password: hashedPassword,
         });
 
         await usersRepository.save(user);
