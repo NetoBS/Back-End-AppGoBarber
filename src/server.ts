675ed9e529c188ae39b66933fdd9a6049b42f31f
Neto Sousa <netosousa29@yahoo.com>
2022-06-20 11:23:44 -0300

    Tratando o back-end do app, Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes'
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -12,6 +13,7 @@ app.get('/', (request, response) => {
 });
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
