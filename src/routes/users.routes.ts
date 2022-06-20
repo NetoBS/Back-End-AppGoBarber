b422194510a1d1733df06b5988deac953fd6f590
Neto Sousa <netosousa29@yahoo.com>
2022-06-20 10:55:35 -0300

    Tratando o back-end do app, Atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadconfig from '../config/upload'
 
 import CreateUserService from '../services/createUsersService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -32,9 +33,23 @@ usersRouter.post('/', async (request, response) =>{
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
-},
+    try {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+            user_id: request.user.id,
+            avatarFilename: request.file.filename,
+        });
+
+        delete user.password
+
+        return response.json(user);
+    } catch (err) {
+        if (err instanceof Error) {
+        return response.status(400).json({ error: err.message });
+        }
+    }
+  },
 );
 
 export default usersRouter;
