ab3947e626428525b70364a5e85a4f6cb92bab8b
Neto Sousa <netosousa29@yahoo.com>
2022-07-01 10:44:47 -0300

    Habilitando CORS na API

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'; 
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes'
@@ -15,6 +16,7 @@ app.get('/', (request, response) => {
     return response.json({ message: 'Hello GoStack '});
 });
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
