7f9602878013cd756a031816719debe11c8909a1
Neto Sousa <netosousa29@yahoo.com>
2022-07-18 23:01:09 -0300

    Organizando alguns arquivos e adicionando camada infra

--- src/modules/appointments/infra/typeorm/entities/Appointment.ts
+++ src/modules/appointments/infra/typeorm/entities/Appointment.ts
@@ -1,6 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn,
      UpdateDateColumn, ManyToOne, JoinColumn} from 'typeorm';
-import User from '../../users/entities/User';
+import User from '../../../../users/infra/typeorm/entities/User';
 
 @Entity('appointments')
 class Appointment {
