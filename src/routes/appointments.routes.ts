ec93d376eb7845db6654e8a636bf761692a28351
Neto Sousa <netosousa29@yahoo.com>
2022-08-20 20:01:27 -0300

    Revertendo commit para arrumar bug

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,44 @@
+import {  Router } from 'express'
+import { getCustomRepository } from 'typeorm';
+import {  parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/createAppointmentServices';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
+
+appointmentsRouter.get('/', async (request, response) => {
+    console.log(request.user);
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+    const appointments = await appointmentsRepository.find();
+
+    return response.json(appointments);
+})
+
+appointmentsRouter.post('/', async (request, response) =>{
+   try {
+        const { provider_id, date } = request.body;
+
+        const parsedDate = parseISO(date);
+
+        const CreateAppointment = new CreateAppointmentService();
+
+        const appointment = await CreateAppointment.execute({ 
+            date: parsedDate,
+            provider_id,
+        });
+        
+
+        return response.json(appointment);
+   } catch (err) {
+        if (err instanceof Error) {
+        return response.status(400).json({ error: err.message });
+        }
+    }
+});
+
+export default appointmentsRouter;
