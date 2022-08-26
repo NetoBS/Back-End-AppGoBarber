cf7313c4671fb58e61f69dd74d5408ad46e13995
Neto Sousa <netosousa29@yahoo.com>
2022-08-26 12:21:44 -0300

    Reescrevendo repositórios

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
 import Appointment from "../../../models/Appointment";
+import ICreateAppointmentDTO from "../dtos/ICreateApointmentDTO";
 
 export default interface IAppointmentsRepository {
+    create(date: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>
 }
