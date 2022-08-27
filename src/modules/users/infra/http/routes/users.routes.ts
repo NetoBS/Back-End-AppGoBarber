8bc8353c1ed409de88f8818cbdd776cba4a0a361
Neto Sousa <netosousa29@yahoo.com>
2022-08-27 17:25:29 -0300

    Criando Injeção de dependências para o users

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import {  Router } from 'express';
 import multer from 'multer';
-import uploadconfig from '@config/upload'
+import uploadconfig from '@config/upload';
+import { container } from 'tsyringe';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import CreateUserService from '@modules/users/services/createUsersService';
@@ -14,8 +15,7 @@ const upload = multer(uploadconfig);
 usersRouter.post('/', async (request, response) =>{
         const { name, email, password } = request.body;
 
-        const usersRepository = new UsersRepository();
-        const createUser = new CreateUserService(usersRepository);
+        const createUser = container.resolve(CreateUserService);
 
         const user = await createUser.execute({
             name,
@@ -28,9 +28,12 @@ usersRouter.post('/', async (request, response) =>{
         return response.json(user);
 });
 
-usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-        const usersRepository = new UsersRepository();
-        const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
+usersRouter.patch(
+    '/avatar', 
+    ensureAuthenticated, 
+    upload.single('avatar'), 
+    async (request, response) => {
+        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
 
         const user = await updateUserAvatar.execute({
             user_id: request.user.id,
