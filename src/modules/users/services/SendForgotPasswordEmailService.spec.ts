851b928dca275e0a368aa21283b7463c47d329b6
Neto Sousa <netosousa29@yahoo.com>
2022-10-19 18:58:36 -0300

    Testes de envio de email

--- src/modules/users/services/SendForgotPasswordEmailService.spec.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -1,21 +1,32 @@
-import AppError from '@shared/errors/AppError';
+//import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
 import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
 
 describe('SendForgotPasswordEmail', () => {
     it('should be able to recover the password using the email', async () => {
         const fakeUsersRepository = new FakeUsersRepository();
-        
-        const createUser = new SendForgotPasswordEmailService(fakeUsersRepository);
+        const fakeMailProvider = new FakeMailProvider();
 
-        await createUser.execute({
+        const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+        
+        const SendForgotPasswordEmail = new SendForgotPasswordEmailService(
+            fakeUsersRepository,
+            fakeMailProvider,
+        );
+
+        await fakeUsersRepository.create({
             name: 'John Doe',
             email: 'johndoe@example.com',
             password: '123456',
         });
 
-        expect(user).toHaveProperty('id');
+        await SendForgotPasswordEmail.execute({
+            email: 'johndoe@example.com',
+        });
+
+        expect(sendMail).toHaveBeenCalled();
     });
 });
 
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 //import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 
 //import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -14,9 +15,17 @@ class SendForgotPasswordEmailService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('MailProvider')
+        private mailProvider: IMailProvider,
     ) {}
 
-    public async execute(): Promise<void> {}
+    public async execute({ email }: IRequest): Promise<void> {
+        this.mailProvider.sendMail(
+            email,
+            'Pedido de recuperação de senha recebido.',
+        );
+    }
 }
 
 export default SendForgotPasswordEmailService;
